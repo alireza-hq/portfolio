a72898d150fa991a236172f45772e24a2e68049d
ItzDoctorMine <haghighi142@gmail.com>
2026-05-30 11:24:24 +0330

    feat: add animated background

---
+++ src/components/layout/AnimatedBackground.tsx
@@ -0,0 +1,31 @@
+const particles = [
+  { text: 'npm run dev', className: 'top-[14%] left-[8%] delay-0' },
+  { text: '<React />', className: 'top-[26%] right-[10%] delay-2' },
+  { text: 'const craft = true', className: 'bottom-[24%] left-[12%] delay-4' },
+  { text: 'git commit -m glow', className: 'right-[16%] bottom-[14%] delay-6' },
+]
+
+export function AnimatedBackground() {
+  return (
+    <div
+      className='portfolio-background pointer-events-none fixed inset-0 -z-10 overflow-hidden bg-zinc-950'
+      aria-hidden='true'
+    >
+      <div className='portfolio-background__aura portfolio-background__aura--violet' />
+      <div className='portfolio-background__aura portfolio-background__aura--cyan' />
+      <div className='portfolio-background__aura portfolio-background__aura--amber' />
+      <div className='portfolio-background__grid' />
+      <div className='portfolio-background__scanline' />
+      <div className='portfolio-background__vignette' />
+
+      {particles.map((particle) => (
+        <span
+          key={particle.text}
+          className={`portfolio-background__particle ${particle.className}`}
+        >
+          {particle.text}
+        </span>
+      ))}
+    </div>
+  )
+}
